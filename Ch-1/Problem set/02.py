7a4e95dbada00b19b665d3b346dd7a01a0e88cbb
Mahak Kushwaha <mahak1905050100030@gmail.com>
2025-09-01 23:39:26 +0530

    day 1

---
+++ Ch-1/Problem set/02.py
@@ -0,0 +1,24 @@
+# 2. Use REPL and print the table of 5 using it.(CMD)
+# >>> print(5*3)
+# 15
+# >>> print(5*1)
+# 5
+# >>> print(5*2)
+# 10
+# >>> print(5*3)
+# 15
+# >>> print(5*4)
+# 20
+# >>> print(5*5)
+# 25
+# >>> print(5*6)
+# 30
+# >>> print(5*7)
+# 35
+# >>> print(5*8)
+# 40
+# >>> print(5*9)
+# 45
+# >>> print(5*10)
+# 50
+# >>>
