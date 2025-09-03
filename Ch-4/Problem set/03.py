b4f1eef53264bcf7a8a7e72ac052a2018d7edaf7
Mahak Kushwaha <mahak1905050100030@gmail.com>
2025-09-03 16:17:01 +0530

    Day-4

---
+++ Ch-4/Problem set/03.py
@@ -0,0 +1,4 @@
+# 3. Check that a tuple type cannot be changed in python.
+a = (34, 234, "Harry")
+
+a[2] = "Larry"
---
+++ Ch-4/four.py
@@ -0,0 +1,44 @@
+# -----------------------------------------------LISTS AND TUPLES-------------------------------------------------------
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(type(friends))
+
+# -----------------------------------------------LIST INDEXING-------------------------------------------------------
+print(friends[0])
+friends[0] = "Grapes" # Unlike Strings lists are mutable
+
+print(friends[0])
+print(friends[1:4])
+
+# -----------------------------------------------LIST METHODS.-------------------------------------------------------
+
+print(friends)
+friends.append("Harry")
+print(friends)
+
+l1 = [1, 34,62, 2, 6, 11]
+l1.sort()
+l1.reverse()
+l1.insert(2, 333333) # Insert 333333 such that its index in the list is 3
+print(l1)
+value = l1.pop(3)
+print(value)
+
+l1.remove(2)
+print(l1)
+
+# -----------------------------------------------TUPLES IN PYTHON-------------------------------------------------------
+a = () # empty tuple
+a = (1,) #tuple with only one element needs a comm
+a = (1,45,342,3424,False, "Rohan", "Shivam") # tuple with more than one element
+print(a)
+print(type(a))
+
+
+# -----------------------------------------------TUPLE METHODS-------------------------------------------------------
+no = a.count(45)
+print(no)
+
+i = a.index(3424)
+print(i)
+
+print(len(a))
